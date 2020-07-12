c00547434b28e0182722a879fca28e8e2e787431
ImedzenneD <imedzenned@yahoo.fr>
2020-07-12 20:27:47 +0100

    Create, Update, and Delete Posts

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField, PasswordField
+from wtforms import StringField, SubmitField, BooleanField, PasswordField, TextAreaField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blog.models import User, Post
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(),Length(min=2 , max=20)])
@@ -28,3 +30,30 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
 
     submit = SubmitField('Login')
+
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=2 , max=20)])
+    email = StringField('Email', validators =[DataRequired(),Email()])
+
+    picture = FileField('Update Profile Picture ', validators=[FileAllowed(['jpg','png'])])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self,username):
+        if username.data != current_user.username: 
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Choose Anather')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Choose Anather')
+
+class PostForm(FlaskForm):
+    title = StringField('Title',validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Submlit')
