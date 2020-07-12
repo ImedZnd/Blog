63c8eb15df8d0494b3e970ebae103b1a512eefae
ImedzenneD <imedzenned@yahoo.fr>
2020-07-12 15:37:28 +0100

    Adding User Authentication

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from blog.models import User, Post
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(),Length(min=2 , max=20)])
@@ -10,7 +11,15 @@ class RegistrationForm(FlaskForm):
 
     submit = SubmitField('Sign Up')
 
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Choose Anather')
 
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Choose Anather')
 class LoginForm(FlaskForm):
 
     email = StringField('Email', validators =[DataRequired(),Email()])
