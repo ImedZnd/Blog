8dfa9f38e77a6d6716ef6f19a5bda2a58e3f1ac3
ImedzenneD <imedzenned@yahoo.fr>
2020-07-14 13:37:59 +0100

    Adding Custom Error Pages

--- blog/__init__.py
+++ blog/__init__.py
@@ -23,6 +23,7 @@ def create_app(config_class=Config):
     from blog.users.routes import users
     from blog.posts.routes import posts
     from blog.main.routes import main
+    from blog.errors.handlers import errors
 
     mail.init_app(app)
     bcrypt.init_app(app)
@@ -32,5 +33,6 @@ def create_app(config_class=Config):
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
