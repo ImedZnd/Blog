78cd05e3db5902e34d36612d32b91c74054dce49
ImedzenneD <imedzenned@yahoo.fr>
2020-07-14 19:38:52 +0100

    Update config.py

--- blog/config.py
+++ blog/config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config :
-    SECRET_KEY='a3ab9ba58d166b8064117b6f628da7a7'
-    SQLALCHEMY_DATABASE_URI= 'sqlite:///site.db'
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI= os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
